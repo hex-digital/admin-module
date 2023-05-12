746fa20e763c3d1f08ac3b8a1d2c717dc73457c4
Ben Sherred <ben@sherred.co.uk>
2023-05-12 12:07:52 +0100

    feat: add the ability to invite admins

---
+++ src/Notifications/InviteAdminNotification.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace HexDigital\ApiConsoleModule\Notifications;
+
+use HexDigital\ApiConsoleModule\Models\Admin;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class InviteAdminNotification extends Notification
+{
+    public function __construct(
+        protected Admin $invitedBy,
+    ) {
+    }
+
+    public function via(Admin $admin): array
+    {
+        return ['mail'];
+    }
+
+    public function toMail(Admin $admin): MailMessage
+    {
+        /** @var string $appName */
+        $appName = config(key: 'app.name', default: '');
+
+        return (new MailMessage())
+            ->subject(subject: "You've been invited to join {$appName}")
+            ->greeting(greeting: "Hi {$admin->first_name},")
+            ->line(line: "You've been invited by {$this->invitedBy->first_name} to join {$appName} as an admin.")
+            ->line(line: 'To get started, please click the button below where you will be asked to reset your account password.')
+            ->action(text: 'Get Started', url: route(name: 'password.request'))
+            ->line(line: "Don't know what this is about? You can safely ignore this email.");
+    }
+}
